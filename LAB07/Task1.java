207562f3c41af26902aad6cd9cda7c2c0b0f8720
Vodiq <paliichuk.vladyslav@chnu.edu.ua>
2023-04-06 01:59:20 +0300

    Застосування ананімних вкладених класів

---
+++ LAB07/Task1.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class Task1 extends JFrame {
+    JTextField input1 = new JTextField(10);
+    JTextField input2 = new JTextField(10);
+    JLabel text1 = new JLabel("Input");
+    JLabel text2 = new JLabel("Output");
+    JButton btn = new JButton("Copy");
+    Task1() {
+        super("Слухачі (listeners) полів та кнопок");
+        try {
+
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+            ;
+        }
+        catch(Exception e) {
+        }
+        setSize(300, 150);
+
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+
+        panel1.add(text1, BorderLayout.WEST);
+        panel1.add(input1, BorderLayout.EAST);
+
+        panel2.add(text2, BorderLayout.WEST);
+        panel2.add(input2, BorderLayout.EAST);
+
+        Container c = getContentPane();
+        c.add(panel1, BorderLayout.NORTH);
+        c.add(panel2, BorderLayout.CENTER);
+        c.add(btn, BorderLayout.SOUTH);
+
+        /*input2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                input2.setText("Введений текст:"+input1.getText());
+            }
+        });*/
+        btn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                input2.setText(input1.getText());
+            }
+        });
+        WindowListener wndCloser = new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        };
+        addWindowListener(wndCloser);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Task1();
+    }
+}
